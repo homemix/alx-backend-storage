c2befce2a329d88d0807900309d781004d027111
homemix <kenmutati@gmail.com>
2022-06-29 18:41:24 +0300

    0. We are all unique!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,4 @@
 -- A query to create table users
--- with unique username and email
 CREATE TABLE IF NOT EXISTS users(
         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
         email VARCHAR(255) NOT NULL UNIQUE,
