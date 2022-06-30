a8e7c95ec9e1e3cfffeacbb998aaf51c653cf258
homemix <kenmutati@gmail.com>
2022-06-30 17:58:12 +0300

    7. Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,7 +3,8 @@ DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    SET @average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id);
+    DECLARE average_score FLOAT;
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id);
     UPDATE users SET average_score = average_score WHERE user_id = user_id;
 END $$;
 DELIMITER ;
