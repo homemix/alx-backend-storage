71582c70e70d96800b8527466967184262c09238
homemix <kenmutati@gmail.com>
2022-07-05 15:08:33 +0300

    Fix: pycodestyle formating 12. Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" MongoDB Operations with Python using pymongo """
+""" MongoDB Operations with Python using  pymongo """
 from pymongo import MongoClient
 
 if __name__ == "__main__":
