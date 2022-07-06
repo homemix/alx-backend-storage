dbb7119c8912d457695b2e94b2bcd42e22349bf4
homemix <kenmutati@gmail.com>
2022-07-06 17:18:19 +0300

    3. Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -26,6 +26,25 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """ Decorator to store the history of inputs and
+    outputs for a particular function.
+    """
+    key = method.__qualname__
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):  # sourcery skip: avoid-builtin-shadow
+        """ Wrapper for decorator functionality """
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(outputs, str(data))
+        return data
+
+    return wrapper
+
+
 class Cache:
     """
     get the value from the cache
@@ -39,6 +58,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         store the data in the cache
